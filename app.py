c55a7986cd587932567b81581fb30fb3b730b13e
Amratef0 <Amr45409@gmail.com>
2026-05-01 16:14:43 +0300

    Last new update

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def index():
     FROM products p
     JOIN categories c ON p.category_id = c.category_id
     ORDER BY p.product_id DESC
-    LIMIT 8
+    LIMIT 4
 """)
     products = cursor.fetchall()
     db.close()
